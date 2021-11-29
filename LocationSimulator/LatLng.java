85e9813ed49dea99b30a61893f02b519f176a4ba
Eins21 <87663682+Eins21@users.noreply.github.com>
2021-11-30 01:10:53 +0530

    Create LatLng.java

---
+++ LocationSimulator/LatLng.java
@@ -0,0 +1,12 @@
+package com.LocationSimulator;
+
+public class LatLng {
+
+	double latitude, longitude;
+	
+	public LatLng(double d, double e) {
+		this.latitude = d;
+		this.longitude = e;
+	}
+
+}
